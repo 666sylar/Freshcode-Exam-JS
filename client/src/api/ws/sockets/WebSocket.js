c74da2b6b09b1aeafc8e05469c4a5e730595e1d9
Nikita Kravchenko <nikita.kravchenko@pm.me>
2025-04-04 14:49:32 +0300

    Refactor WebSocket

--- client/src/api/ws/sockets/WebSocket.js
+++ client/src/api/ws/sockets/WebSocket.js
@@ -1,19 +1,22 @@
-import socketIoClient from 'socket.io-client';
+import { io } from 'socket.io-client';
 import CONSTANTS from '../../../constants';
 
 class WebSocket {
   constructor (dispatch, getState, room) {
     this.dispatch = dispatch;
     this.getState = getState;
-    this.socket = socketIoClient(`${CONSTANTS.BASE_URL}${room}`, {
-      origins: 'localhost:*',
-    });
+    this.socket = io(`ws://localhost:3000/${room}`);
     this.listen();
   }
 
   listen = () => {
     this.socket.on('connect', () => {
+      console.log('WebSocket connected');
       this.anotherSubscribes();
+    });
+
+    this.socket.on('connect_error', error => {
+      console.log('WebSocket connection error:', error);
     });
   };
 
--- server/src/index.js
+++ server/src/index.js
@@ -6,8 +6,7 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+controller.createConnection(server);
 server.listen(PORT, () =>
   console.log(`Example app listening on port ${PORT}!`)
 );
-
-controller.createConnection(server);
